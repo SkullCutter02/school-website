bfe6052124e993f4d61d07ca08b10152ab8ed3ac
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2021-03-26 15:15:09 +0800

    Tested /auth routes

--- api/src/tests/auth.test.ts
+++ api/src/tests/auth.test.ts
@@ -17,22 +17,71 @@ afterAll(async () => {
   await tearDownDatabase();
 });
 
-describe("POST /auth/login", () => {
-  it("should not return a user object", async () => {
-    const res = await request(server).post("/auth/login").send({ username: "admin", password: "dwdw" });
+describe("auth route", () => {
+  describe("POST /auth/login", () => {
+    it("should not return a user object because incorrect password", async () => {
+      const res = await request(server).post("/auth/login").send({ username: "admin", password: "dwdw" });
 
-    expect(res.status).toEqual(403);
+      expect(res.status).toEqual(403);
+    });
+
+    it("should return a user object", async () => {
+      const res = await request(server)
+        .post("/auth/login")
+        .send({ username: "admin", password: process.env.ADMIN_PASSWORD });
+
+      expect(res.status).toEqual(200);
+      expect(res.body.username).toEqual("admin");
+      expect(res.body.createdAt).toBeTruthy();
+      expect(res.body.updatedAt).toBeTruthy();
+      expect(res.body.uuid).toBeTruthy();
+    });
+
+    it("should not return a user object because incorrect username", async () => {
+      const res = await request(server).post("/auth/login").send({ username: "bla", password: "dwdw" });
+
+      expect(res.status).toEqual(500);
+    });
   });
 
-  it("should return a user object", async () => {
-    const res = await request(server)
-      .post("/auth/login")
-      .send({ username: "admin", password: process.env.ADMIN_PASSWORD });
+  describe("POST /auth/logout", () => {
+    it("should return logout successful", async () => {
+      const login = await request(server)
+        .post("/auth/login")
+        .send({ username: "admin", password: process.env.ADMIN_PASSWORD });
 
-    expect(res.status).toEqual(200);
-    expect(res.body.username).toEqual("admin");
-    expect(res.body.createdAt).toBeTruthy();
-    expect(res.body.updatedAt).toBeTruthy();
-    expect(res.body.uuid).toBeTruthy();
+      const res = await request(server).post("/auth/logout").set("Cookie", login.header["set-cookie"]);
+
+      expect(res.status).toEqual(200);
+      expect(res.body.logout).toEqual("Successful");
+    });
+
+    it("should return logout failed", async () => {
+      const res = await request(server).post("/auth/logout");
+
+      expect(res.status).toEqual(403);
+    });
+  });
+
+  describe("POST /auth/refresh", () => {
+    it("should return a user object", async () => {
+      const login = await request(server)
+        .post("/auth/login")
+        .send({ username: "admin", password: process.env.ADMIN_PASSWORD });
+
+      const res = await request(server).post("/auth/refresh").set("Cookie", login.header["set-cookie"]);
+
+      expect(res.status).toEqual(200);
+      expect(res.body.username).toBeTruthy();
+      expect(res.body.createdAt).toBeTruthy();
+      expect(res.body.updatedAt).toBeTruthy();
+      expect(res.body.uuid).toBeTruthy();
+    });
+
+    it("should not return a user object", async () => {
+      const res = await request(server).post("/auth/refresh");
+
+      expect(res.status).toEqual(403);
+    });
   });
 });

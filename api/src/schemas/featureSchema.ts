c74937e69eed6faef90abec8a537e8747052ab3e
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2021-03-29 10:58:53 +0800

    Changed feature schema

--- api/src/schemas/featureSchema.ts
+++ api/src/schemas/featureSchema.ts
@@ -1,8 +1,8 @@
 import * as yup from "yup";
 
 export const createFeatureSchema = yup.object({
-  title: yup.string().min(1).max(20).required(),
-  body: yup.string().min(0).max(200),
+  title: yup.string().min(1).max(50).required(),
+  body: yup.string().min(0).max(500),
 });
 
 export const patchFeatureSchema = yup.object({
